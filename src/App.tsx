08302a444deedebfd10963f8e786765f312b99e8
Juan Herrera <juanherrera@Juans-MacBook-Pro.local>
2024-06-18 00:50:24 -0600

    Add sounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { PADS_INFO } from "./utils/constants";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface PadProps {
+  id: string;
+  codeKey: string | number;
+  url: string;
+  keyButton: string;
 }
 
-export default App
+const onHandleClick = (url: string) => {
+  new Audio(url).play();
+};
+
+const onPressClick = (e, codeKey, url) => {
+  if (e.key === codeKey) {
+    new Audio(url).play();
+  }
+};
+
+const Pad = ({ url, id, codeKey, keyButton }: PadProps) => {
+  return (
+    <div id={id}>
+      <button onClick={() => onHandleClick(url)}>
+        {keyButton}
+        <audio id={keyButton} src={url} />
+      </button>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div id="drum-machine">
+      <h1>DRUM MACHINE</h1>
+      <div id="display">
+        {PADS_INFO.map(pad => (
+          <Pad
+            id={pad.id}
+            keyButton={pad.keyButton}
+            url={pad.url}
+            key={pad.keyButton}
+            codeKey={pad.codeKey}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/constants.ts
@@ -0,0 +1,56 @@
+export const PADS_INFO = [
+  {
+    codeKey: 81,
+    keyButton: "Q",
+    id: "heater1",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3",
+  },
+  {
+    codeKey: 87,
+    keyButton: "W",
+    id: "heater2",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3",
+  },
+  {
+    codeKey: 69,
+    keyButton: "E",
+    id: "heater3",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3",
+  },
+  {
+    codeKey: 65,
+    keyButton: "A",
+    id: "heater4",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3",
+  },
+  {
+    codeKey: 83,
+    keyButton: "S",
+    id: "clap",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3",
+  },
+  {
+    codeKey: 68,
+    keyButton: "D",
+    id: "openHH",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3",
+  },
+  {
+    codeKey: 90,
+    keyButton: "Z",
+    id: "kickNHat",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3",
+  },
+  {
+    codeKey: 88,
+    keyButton: "X",
+    id: "kick",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3",
+  },
+  {
+    codeKey: 67,
+    keyButton: "C",
+    id: "closedHH",
+    url: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3",
+  },
+];
